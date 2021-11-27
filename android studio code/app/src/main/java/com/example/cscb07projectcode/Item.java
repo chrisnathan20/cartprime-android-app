077326ac1d9a57babe372bf9a08d463e0b050fe7
Varun <varun.datta180202@gmail.com>
2021-11-27 18:33:07 -0500

    Added a new Java Class called Item

---
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/Item.java
@@ -0,0 +1,81 @@
+package com.example.cscb07projectcode;
+
+public class Item {
+
+    private String name;
+    private double price;
+    private int quantity;
+    private String unit;
+
+
+    public Item() {
+
+    }
+
+    public Item(String name, double price, int quantity, String unit) {
+        setName(name);
+        setQuantity(quantity);
+        setUnit(unit);
+        setPrice(price);
+
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public String getUnit() {
+        return this.unit;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Item Name: " + this.name + " Quantity: " + this.quantity + " " + this.unit + "for $" + this.price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        if (o.getClass() != this.getClass())
+            return false;
+        Item obj = (Item) o;
+        if (obj.getName() != this.name && obj.getPrice() != this.price && obj.getQuantity() != this.quantity && this.unit != obj.getUnit())
+            return false;
+        return true;
+    }
+
+    public int hashCode() {
+        int x = 0;
+        int p = (int) this.price;
+        x = this.quantity + p ;
+        return x;
+    }
+
+}
