e756ee883cb662373316b647b7a83cf85c7c3aad
Christopher Nathanael <chrisnathan20@gmail.com>
2021-11-29 00:13:19 -0500

    fill LoginContract interface

--- android studio code/app/src/main/java/com/example/cscb07projectcode/LoginContract.java
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/LoginContract.java
@@ -1,4 +1,18 @@
 package com.example.cscb07projectcode;
 
 public interface LoginContract {
+    public interface Model{
+        public boolean userExists(String username);
+        public boolean correctPassword(String username);
+    }
+
+    public interface View {
+        public String getUsername();
+        public String getPassword();
+        public void displayMessage(String message);
+    }
+
+    public interface Presenter {
+        public void checkLogin();
+    }
 }
