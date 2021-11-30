5f2581e5d114dcc2101d01386fa89aa1bf683fce
Harrick Cheong <harrickcheong@gmail.com>
2021-11-30 00:32:31 -0500

    implemented database connection for storeowner products, fixed minor intent bug

--- android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/StoreOwnerMainActivity.java
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/StoreOwnerMainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cscb07projectcode.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,8 +14,16 @@ import android.view.View;
 import com.example.cscb07projectcode.Item;
 import com.example.cscb07projectcode.R;
 import com.example.cscb07projectcode.ProductRecyclerAdapter;
+import com.example.cscb07projectcode.Store;
+import com.example.cscb07projectcode.StoreOwner;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StoreOwnerMainActivity extends AppCompatActivity {
 
@@ -37,8 +46,61 @@ public class StoreOwnerMainActivity extends AppCompatActivity {
     }
 
     public void setStoreInfo() {
-        itemsList.add(new Item("Walmart", "Retail", 24.99, 35, "kg"));
+        // Get the Intent that started this activity and extract the username
+        String username = getIntent().getStringExtra(LoginStoreOwnerActivity.username_key);
+
+        // leverages store owner's username to find their store name
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("stores").child("list_of_stores");
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // loops through user (of type store owner) until it matches a username
+                for(DataSnapshot child:dataSnapshot.getChildren()) {
+                    Store store = child.getValue(Store.class);
+                    if(username.equals(store.getStoreowner())){
+                        Log.i("mytag", "passed into storeowner");
+                        // create a database reference to access list of products
+                        DatabaseReference newRef = ref.child(store.getName()).child("products");
+                        ValueEventListener newListener = new ValueEventListener(){
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                // iterate through each product and append it into the arraylist
+                                for(DataSnapshot newChild:snapshot.getChildren()) {
+                                    Item item = newChild.getValue(Item.class);
+                                    itemsList.add(item);
+                                    Log.i("mytag", item.getName());
+                                    Log.i("mytag", item.getDescription());
+
+                                }
+//                                Log.i("mytag", itemsList.toString());
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        };
+//                        // store hashmap into a variable
+//                        Object products = child.getValue();
+//                        HashMap other = (HashMap) products;
+////                        Log.i("mtag",child.getValue().get("products").getClass().toString());
+//                        Log.i("mtag",child.getValue().toString());
+//                        Log.i("mtag",other.get("products").toString());
+                        newRef.addValueEventListener(newListener);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("warning", "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        ref.addValueEventListener(listener);
+//        itemsList.add(new Item("test", "test", 25.44, 34, "test"));
+
     }
+
 
     public void setAdapter(){
         ProductRecyclerAdapter adapter = new ProductRecyclerAdapter(itemsList);
