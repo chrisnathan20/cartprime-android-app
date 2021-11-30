cb07bd5ea3043ab0a1bc371275fed6e44c2a32ae
Christopher Nathanael <chrisnathan20@gmail.com>
2021-11-30 01:09:08 -0500

    implemented customer mvp login

--- android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/LoginCustomerActivity.java
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/LoginCustomerActivity.java
@@ -5,15 +5,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.cscb07projectcode.CustomerModel;
+import com.example.cscb07projectcode.LoginContract;
+import com.example.cscb07projectcode.LoginPresenter;
 import com.example.cscb07projectcode.R;
+import com.example.cscb07projectcode.StoreOwnerModel;
 
-public class LoginCustomerActivity extends AppCompatActivity {
+public class LoginCustomerActivity extends AppCompatActivity  implements LoginContract.View {
+
+    public static final String username_key = "username_key";
+
+    private LoginContract.Presenter presenter;
+
+    public String getUsername(){
+        EditText editText = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        return editText.getText().toString();
+    }
+
+    public String getPassword(){
+        EditText editText = (EditText)  findViewById(R.id.editTextTextPassword2);
+        return editText.getText().toString();
+    }
+
+    public void displayMessage(String message) {
+        TextView alert = (TextView) findViewById(R.id.TextViewAlert);
+        alert.setText(message);
+    }
+
+
+    public void onSuccess(String username) {
+        Intent intent = new Intent(this, CustomerMainActivity.class);
+
+        // pass data through intent into the next activity, which is the store owner's menu page
+        intent.putExtra(username_key, username);
+
+        // navigate to the next activity
+        startActivity(intent);
+    }
+
+    public void login_button (View view){
+        //commented out, intent passing implemented in method onSuccess
+        /*
+        Intent intent = new Intent(this, StoreOwnerMainActivity.class);
+
+        // pass data through intent into the next activity, which is the store owner's menu page
+        EditText editText = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        String username = editText.getText().toString();
+        intent.putExtra(username_key, username);
+
+        // navigate to the next activity
+        startActivity(intent);
+         */
+
+        presenter.checkLogin();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_customer);
+        presenter = new LoginPresenter(new CustomerModel(), this);
     }
 
     public void register_button (View view){
@@ -21,7 +75,7 @@ public class LoginCustomerActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void login_button (View view){
+    public void no_authorization_login (View view){
         Intent intent = new Intent(this, CustomerMainActivity.class);
         startActivity(intent);
     }
--- android studio code/app/src/main/java/com/example/cscb07projectcode/CustomerModel.java
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/CustomerModel.java
@@ -1,4 +1,83 @@
 package com.example.cscb07projectcode;
 
-public class CustomerModel {
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class CustomerModel implements LoginContract.Model {
+
+    /*
+    //to work around the void return of onDataChange
+    public static boolean user_result = false;
+    public static boolean pw_result = false;
+     */
+
+    public CustomerModel() {
+    }
+
+    public void userCheck(String username, String password, LoginContract.Presenter presenter) {
+
+        DatabaseReference root = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference user = root.child("users").child("customers").child(username);
+
+        user.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if(!snapshot.exists()){
+                    presenter.invalidLogin();
+                }
+                else{
+                    passwordCheck(username, password, presenter);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        /* For some reason, this does not work
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child("storeowners").child(username);
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // true if username exists, false if username does not exists
+                StoreOwnerModel.user_result = snapshot.exists();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        };
+        */
+    }
+
+    public void passwordCheck(String username, String password, LoginContract.Presenter presenter) {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child("customers");
+
+        ref.child(username).child("password").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                //true if password in database is equal to password in argument
+                //false otherwise
+                if(!password.equals(snapshot.getValue())){
+                    presenter.invalidLogin();
+                }
+                else{
+                    presenter.validLogin(username);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError firebaseError) {
+            }
+        });
+
+    }
 }
