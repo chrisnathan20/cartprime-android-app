187e83b6f3d1efca05e56b24a11a5f2ce38e215c
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2021-11-30 12:00:13 -0500

    set up testing environment, initial mockito commit

---
+++ android studio code/app/src/test/java/com/example/cscb07projectcode/PresenterUnitTest.java
@@ -0,0 +1,90 @@
+package com.example.cscb07projectcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.cscb07projectcode.Activities.LoginStoreOwnerActivity;
+import com.example.cscb07projectcode.Activities.StoreOwnerMainActivity;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PresenterUnitTest {
+
+    @Mock
+    LoginStoreOwnerActivity view;
+
+    @Mock
+    StoreOwnerModel model;
+
+    /*@Test
+    public void testStoreOwnerPresenterCorrectUsernameCorrectPassword(){
+        model = Mockito.spy(model);
+        // stubbing
+        when(view.getUsername()).thenReturn("mockito");
+        when(view.getPassword()).thenReturn("testing");
+
+        LoginPresenter presenter = new LoginPresenter(model, view);
+        presenter.checkLogin();
+
+        // verify displayMessage and onSuccess called
+        verify(view).displayMessage("Login Successful.");
+        verify(view).onSuccess("mockito");
+    }*/
+
+    @Test
+    public void testPresenterEmptyUsernameEmptyPassword(){
+        // stubbing
+        when(view.getUsername()).thenReturn("");
+        when(view.getPassword()).thenReturn("");
+
+        LoginPresenter presenter = new LoginPresenter(model, view);
+        presenter.checkLogin();
+
+        //verify correct message is displayed
+        verify(view).displayMessage("Please fill in all input fields.");
+    }
+
+    @Test
+    public void testPresenterNonEmptyUsernameEmptyPassword(){
+        // stubbing
+        when(view.getUsername()).thenReturn("mockito");
+        when(view.getPassword()).thenReturn("");
+
+        LoginPresenter presenter = new LoginPresenter(model, view);
+        presenter.checkLogin();
+
+        //verify correct message is displayed
+        verify(view).displayMessage("Please fill in all input fields.");
+    }
+
+    @Test
+    public void testPresenterEmptyUsernameNonEmptyPassword(){
+        // stubbing
+        when(view.getUsername()).thenReturn("");
+        when(view.getPassword()).thenReturn("testing");
+
+        LoginPresenter presenter = new LoginPresenter(model, view);
+        presenter.checkLogin();
+
+        //verify correct message is displayed
+        verify(view).displayMessage("Please fill in all input fields.");
+    }
+
+}
