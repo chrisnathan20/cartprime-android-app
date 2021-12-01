767a2f0f657242570e974ae50db44865f92cf8c8
Varun <varun.datta180202@gmail.com>
2021-12-01 03:42:19 -0500

    Worked on creating the cart and order generating files

--- android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/CartStuff/StoreFragment.java
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/CartStuff/StoreFragment.java
@@ -1,9 +1,13 @@
 package com.example.cscb07projectcode.Activities.CartStuff;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +17,15 @@ import com.example.cscb07projectcode.Item;
 import com.example.cscb07projectcode.R;
 import com.example.cscb07projectcode.databinding.FragmentStore2Binding;
 
+import java.util.ArrayList;
+
 
 public class StoreFragment extends Fragment implements StoreListAdapter.storeInterace {
 
 FragmentStore2Binding fragmentStore2Binding;
+
 private  StoreListAdapter storeListAdapter;
+private  StoreViewModel storeViewModel;
 
     public StoreFragment() {
         // Required empty public constructor
@@ -40,6 +48,20 @@ private  StoreListAdapter storeListAdapter;
         super.onViewCreated(view,savedInstanceState);
         storeListAdapter = new StoreListAdapter();
         fragmentStore2Binding.storeRecyclerView.setAdapter(storeListAdapter);
+
+        storeViewModel = new ViewModelProvider(requireActivity()).get(StoreViewModel.class);
+
+        SharedPreferences pref = this.getActivity().getSharedPreferences("credentials_store_name", Context.MODE_PRIVATE);
+        String username = pref.getString("store_name", ""); // retrieving the store name
+        storeViewModel.setS(username);
+
+        storeViewModel.getItems().observe(getViewLifecycleOwner(), new Observer<ArrayList<Item>>()
+        {
+            @Override
+            public void onChanged(ArrayList<Item> items) {
+                storeListAdapter.submitList(items);
+            }
+        });
 
     }
 
--- android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/CartStuff/StoreRepo.java
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/CartStuff/StoreRepo.java
@@ -1,4 +1,96 @@
 package com.example.cscb07projectcode.Activities.CartStuff;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.cscb07projectcode.Item;
+import com.example.cscb07projectcode.Store;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 public class StoreRepo {
+        String store;
+        public StoreRepo(String s)
+        {
+            this.store = s;
+        }
+    // load data from firebase onto here
+    // this is our repository
+
+    public MutableLiveData<ArrayList<Item>> mutableItemList;
+    public LiveData<ArrayList<Item>> getItems()
+    {
+        if(mutableItemList == null)
+        {
+            mutableItemList = new MutableLiveData<>();
+            loadItems();
+        }
+        return mutableItemList;
+    }
+
+
+
+
+
+    private void  loadItems() // for encapsulation this is private to prevent unauthorised access to store data
+    {
+        ArrayList<Item> itemArrayList = new ArrayList<Item>();
+
+        //SharedPreferences pref = getSharedPreferences("credentials_store_name", Context.MODE_PRIVATE);
+        //String username = pref.getString("store_name", ""); // retrieving the store name
+        // We will use the databse to populate this list now
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("stores").child("list_of_stores");
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // loops through user (of type store owner) until it matches a username
+                for(DataSnapshot child:dataSnapshot.getChildren()) {
+                    Store store = child.getValue(Store.class);
+                    if(store.equals(store.getName())){
+                        // create a database reference to access list of products
+                        DatabaseReference newRef = ref.child(store.getName()).child("products");
+                        ValueEventListener newListener = new ValueEventListener(){
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                // iterate through each product and append it into the arraylist
+                                for(DataSnapshot newChild:snapshot.getChildren()) {
+                                    Item item = newChild.getValue(Item.class);
+                                    itemArrayList.add(item);
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        };
+                        newRef.addValueEventListener(newListener);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("warning", "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        ref.addValueEventListener(listener);
+
+
+
+
+
+        mutableItemList.setValue(itemArrayList);
+    }
 }
+
