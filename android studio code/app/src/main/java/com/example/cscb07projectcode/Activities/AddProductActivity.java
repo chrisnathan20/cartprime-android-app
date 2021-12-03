b5e5e0f4359f56806bd3723746fefa197d75ae20
Harrick Cheong <harrickcheong@gmail.com>
2021-12-03 12:41:15 -0500

    added authentication for input fields in addproduct activity

--- android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/AddProductActivity.java
+++ android studio code/app/src/main/java/com/example/cscb07projectcode/Activities/AddProductActivity.java
@@ -1,5 +1,6 @@
 package com.example.cscb07projectcode.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.cscb07projectcode.Item;
 import com.example.cscb07projectcode.R;
@@ -30,7 +32,7 @@ public class AddProductActivity extends AppCompatActivity {
     }
 
     // called when publish button is pressed and writes new product data to database
-    public void publish_button(View view){
+    public void publish_button(View view) {
         // Get the Intent that started this activity and extract the username
 //        String username = getIntent().getStringExtra(LoginStoreOwnerActivity.username_key);
         //        Log.i("mytag", username);
@@ -50,50 +52,118 @@ public class AddProductActivity extends AppCompatActivity {
 
         // store user input for product price
         EditText price_id = (EditText) findViewById(R.id.editTextTextPersonName5);
-        double price_field = Double.parseDouble(price_id.getText().toString());
+        String strprice_field = price_id.getText().toString();
 
         // store user input for product quantity
         EditText quantity_id = (EditText) findViewById(R.id.editTextTextPersonName6);
-        int quantity_field = Integer.parseInt(quantity_id.getText().toString());
+        String strquantity_field = quantity_id.getText().toString();
 
         // store user input for product measurement unit
         EditText unit_id = (EditText) findViewById(R.id.editTextTextPersonName9);
         String unit_field = unit_id.getText().toString();
 
-        // leverages store owner's username to find their store name
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child("storeowners");
-        ValueEventListener listener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // loops through user (of type store owner) until it matches a username
-                for(DataSnapshot child:dataSnapshot.getChildren()) {
-                    StoreOwner storeowner = child.getValue(StoreOwner.class);
-                    Log.i("mytag", username);
-                    Log.i("mytag", storeowner.getUsername());
-                    if(username.equals(storeowner.getUsername())){
-                        Log.i("mytag", "works");
-                        // extract store name of storeowner
-                        String storename = storeowner.getStorename();
-                        // create a new product instance
-                        Item item = new Item(productname_field, description_field, price_field, quantity_field, unit_field);
-                        // get database reference object
-                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-                        // append new product under the storeowner's store as a child with key: product name
-                        ref.child("stores").child("list_of_stores").child(storename).child("products").child(productname_field).setValue(item);
-                        Log.i("mytag", storename);
-                        break;
+        // store notification message
+        TextView notifyMessage_id = (TextView) findViewById(R.id.textView26);
+
+        // reset field_status=true on each button click
+        boolean field_status = true;
+
+        if (productname_field.isEmpty()) {
+            field_status = false;
+            productname_id.setError("Please fill in the product's name.");
+        }
+
+        if (description_field.isEmpty()) {
+            field_status = false;
+            description_id.setError("Please fill in the product's description.");
+        }
+
+        if (String.valueOf(strprice_field).isEmpty()) {
+            field_status = false;
+            price_id.setError("Please fill in the price of the product");
+        }
+
+        if (String.valueOf(strquantity_field).isEmpty()) {
+            field_status = false;
+            quantity_id.setError("Please fill in the product's quantity.");
+        }
+
+        if (unit_field.isEmpty()) {
+            field_status = false;
+            unit_id.setError("Please specify in the product's unit type.");
+            Log.i("empty", "unit is empty");
+        }
+
+        // if any input fields are empty
+        if (field_status == false) {
+            notifyMessage_id.setText("Please fill in all input fields");
+            Log.i("empty", "field status is empty");
+        }
+        // if all input fields are filled
+        else {
+            // restore the data types of the variables
+            int quantity_field = Integer.parseInt(strquantity_field);
+            double price_field = Double.parseDouble(strprice_field);
+            // leverages store owner's username to find their store name
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child("storeowners");
+            ValueEventListener listener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // loops through user (of type store owner) until it matches a username
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        StoreOwner storeowner = child.getValue(StoreOwner.class);
+                        Log.i("mytag", username);
+                        Log.i("mytag", storeowner.getUsername());
+                        if (username.equals(storeowner.getUsername())) {
+                            Log.i("mytag", "works");
+                            // extract store name of storeowner
+                            String storename = storeowner.getStorename();
+                            // create a new product instance
+                            Item item = new Item(productname_field, description_field, price_field, quantity_field, unit_field);
+
+                            // validate if product name already exists in the database
+                            DatabaseReference newRef = FirebaseDatabase.getInstance().getReference().child("stores").child("list_of_stores").child(storename).child("products").child(productname_field);
+                            ValueEventListener newListener = new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot newSnapshot) {
+                                    Log.i("exists", newRef.getKey());
+                                    // if product already exists
+                                    if (newSnapshot.exists()) {
+                                        productname_id.setError("This product already exists in the store.");
+
+                                        // store input for message notification;
+                                        notifyMessage_id.setText("This product already exists in the store.");
+                                    }
+                                    // if product does not exist, append and write it to the database
+                                    else {
+                                        // get database reference built on first database reference object
+                                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                                        // append new product under the storeowner's store as a child with key: product name
+                                        ref.child("stores").child("list_of_stores").child(storename).child("products").child(productname_field).setValue(item);
+                                        Log.i("mytag", storename);
+                                        // pass data through intent into the next activity, which is the store owner's menu page
+                                        newIntent.putExtra(username_key, username);
+                                        startActivity(newIntent);
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            };
+                            newRef.addListenerForSingleValueEvent(newListener);
+                            break;
+                        }
                     }
                 }
-                // pass data through intent into the next activity, which is the store owner's menu page
-                newIntent.putExtra(username_key, username);
-                startActivity(newIntent);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.w("warning", "loadPost:onCancelled", databaseError.toException());
-            }
-        };
-        ref.addValueEventListener(listener);
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w("warning", "loadPost:onCancelled", databaseError.toException());
+                }
+            };
+            ref.addValueEventListener(listener);
+        }
     }
 }
